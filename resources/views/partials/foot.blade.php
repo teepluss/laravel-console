8a21cd61ad6f740f22a92432b2dd9f417541ca9b
EThaiZone <ethaizone@hotmail.com>
2016-12-13 16:24:33 +0700

    add custom asset url

--- resources/views/partials/foot.blade.php
+++ resources/views/partials/foot.blade.php
@@ -1,6 +1,6 @@
-		<script src="{{ asset('vendor/console/js/vendor/jquery.js') }}"></script>
-		<script src="{{ asset('vendor/console/js/vendor/plugins.js') }}"></script>
-		<script src="{{ asset('vendor/console/js/vendor/codemirror.js') }}"></script>
-		<script src="{{ asset('vendor/console/js/main.js') }}"></script>
+		<script src="{{ config('console.asset_url') . '/js/vendor/jquery.js' }}"></script>
+		<script src="{{ config('console.asset_url') . '/js/vendor/plugins.js' }}"></script>
+		<script src="{{ config('console.asset_url') . '/js/vendor/codemirror.js' }}"></script>
+		<script src="{{ config('console.asset_url') . '/js/main.js' }}"></script>
 	</body>
 </html>
